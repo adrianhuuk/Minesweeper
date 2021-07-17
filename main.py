d0a3ddb489fb09ad985ce438d2ce596a2af6f443
Adrian <79156275+adrianhuuk@users.noreply.github.com>
2021-07-17 14:29:56 +0100

    All "mines" placed, displayed as binary in nested list.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random as rd
 
 diff = int(input("Enter your difficulty (1-3): "))
+#diff = 1
 
 if diff < 3:
     size = [diff * 8 for i in range(2)]
@@ -15,16 +16,19 @@ else:
 squares = [[0 for i in range(size[0])] for i in range(size[1])]
 
 for i in range(mines):
-    #select = squares[rd.randrange(size[1])][rd.randrange(size[0])]
     select = [rd.randrange(size[1]), rd.randrange(size[0])]
-    if squares[select[0]][select[1]] != 1:
-        select = squares[select[0]][select[1]]
-        print(select)
+    while squares[select[0]][select[1]] == 1:
+        select = [rd.randrange(size[1]), rd.randrange(size[0])]
+    squares[select[0]][select[1]] = 1
 
-total = 0
+#total = 0
 for i in squares:
-    print(i)
-    total += 1
-print(total)
+    line = []
+    for j in i:
+        if j == 1:
+            line.append(1)
+        else:
+            line.append(0)
+    print(line)
 
 
