ed537496065ee7d92046c23f03d105c7a09f7690
Adrian <79156275+adrianhuuk@users.noreply.github.com>
2021-07-17 14:35:59 +0100

    Changed setup of "size" and "mines" variables to use dictionary

--- main.py
+++ main.py
@@ -3,15 +3,10 @@ import random as rd
 diff = int(input("Enter your difficulty (1-3): "))
 #diff = 1
 
-if diff < 3:
-    size = [diff * 8 for i in range(2)]
-    if size == 1:
-        mines = 10
-    else:
-        mines = 40
-else:
-    size = [16, 30]
-    mines = 99
+setup = {1:[10, [8, 8]], 2:[40, [16, 16]], 3:[99, [16, 30]]}
+
+size = setup[diff][1]
+mines = setup[diff][0]
 
 squares = [[0 for i in range(size[0])] for i in range(size[1])]
 
@@ -21,7 +16,6 @@ for i in range(mines):
         select = [rd.randrange(size[1]), rd.randrange(size[0])]
     squares[select[0]][select[1]] = 1
 
-#total = 0
 for i in squares:
     line = []
     for j in i:
