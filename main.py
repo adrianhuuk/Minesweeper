443b1301ea65bc16779ee54c8888e51b36a446ce
Adrian <79156275+adrianhuuk@users.noreply.github.com>
2021-07-27 13:28:19 +0100

    Moved setup and display features to Grid Class.
    Class methods now used to print different variations of grid.
    
    Tidied up unneccesary commented code.

--- main.py
+++ main.py
@@ -3,49 +3,53 @@
 import random as rd
 
 diff = int(input("Enter your difficulty (1-3): "))
-#diff = 2
 
-# Setup Script
+# Grid Class Script
 
-setup = {1:[10, [8, 8]], 2:[40, [16, 16]], 3:[99, [16, 30]]}
+class Grid:
+    def __init__(self):
+        
+        # Mines Grid Setup
 
-size = setup[diff][1]
-mines = setup[diff][0]
+        setup = {1:[10, [8, 8]], 2:[40, [16, 16]], 3:[99, [16, 30]]}
 
-squares = [[0 for i in range(size[0])] for i in range(size[1])]
+        self.size = setup[diff][1]
+        self.mines = setup[diff][0]
 
-for i in range(mines):
-    select = [rd.randrange(size[1]), rd.randrange(size[0])]
-    while squares[select[0]][select[1]] == 1:
-        select = [rd.randrange(size[1]), rd.randrange(size[0])]
-    squares[select[0]][select[1]] = 1
+        self.squares = [[0 for i in range(self.size[0])] for i in range(self.size[1])]
 
+        for i in range(self.mines):
+            select = [rd.randrange(self.size[1]), rd.randrange(self.size[0])]
+            while self.squares[select[0]][select[1]] == 1:
+                select = [rd.randrange(self.size[1]), rd.randrange(self.size[0])]
+            self.squares[select[0]][select[1]] = 1
+        
+            # Displayed Grid Setup
 
-# for i in squares:
-#      line = []
-#      for j in i:
-#          if j == 1:
-#              line.append(1)
-#          else:
-#              line.append(0)
-#      print(line)
+        self.display = []
 
-# Display Script
+        for i in range(len(self.squares)):
+                line = []
+                for j in range(len(self.squares[i])):
+                    #line.append((self.squares[i-1][j-1] + self.squares[i-1][j] + self.squares[i-1][j+1]) + (self.squares[i][j-1] + self.squares[i][j+1]) + (self.squares[i+1][j-1] + self.squares[i+1][j] + self.squares[i+1][j+1]))
+                    temp = 0
+                    for k in range(3):
+                        for l in range(3):
+                            if not (k == 1 and l == 1) and not (i == 0 and k == 0):
+                                try:
+                                    temp += self.squares[i+(k-1)][j+(l-1)]
+                                except IndexError:
+                                    pass
+                    line.append(temp)
+                self.display.append(line)
+        # Other Setup
 
-display = []
-for i in range(len(squares)):
-    line = []
-    for j in range(len(squares[i])):
-        #line.append((squares[i-1][j-1] + squares[i-1][j] + squares[i-1][j+1]) + (squares[i][j-1] + squares[i][j+1]) + (squares[i+1][j-1] + squares[i+1][j] + squares[i+1][j+1]))
-        temp = 0
-        for k in range(3):
-            for l in range(3):
-                if not (k == 1 and l == 1) and not (i == 0 and k == 0):
-                    try:
-                        temp += squares[i+(k-1)][j+(l-1)]
-                    except IndexError:
-                        pass
-        line.append(temp)
-    display.append(line)
-for i in display:
-    print(i)
+        self.gridKey = {"d":self.display, "m":self.squares}
+
+    def prt(self, type):
+        for i in self.gridKey[type]:
+            print(i)
+        
+dGrid = Grid()
+
+dGrid.prt("d")
